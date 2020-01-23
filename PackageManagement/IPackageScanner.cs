0f916e35ab700ce6f681de01f7e25188db91bd45
radujipa <radu.t.jipa@gmail.com>
2020-01-23 13:51:26 +0000

    Add library for package management
    At the moment, this only contains a helper function to allow detection
    of apps being installed. In the future it may contain more such helper
    functions to make working with UWP packages easier.

---
+++ PackageManagement/IPackageScanner.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace PackageManagement
+{
+    public interface IPackageScanner
+    {
+        Task<bool> IsAppInstalledAsync(Uri protocolName, string packageFamilyName);
+    }
+}
---
+++ PackageManagement/PackageScanner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.System;
+
+
+namespace PackageManagement
+{
+    public class PackageScanner : IPackageScanner
+    {
+        public async Task<bool> IsAppInstalledAsync(Uri protocolName, string packageFamilyName)
+        {
+            try
+            {
+                LaunchQuerySupportStatus status = 
+                    await Launcher.QueryUriSupportAsync(
+                        protocolName, LaunchQuerySupportType.Uri, packageFamilyName);
+
+                bool appInstalled = status == LaunchQuerySupportStatus.Available;
+                
+                Debug.WriteLine(
+                    "PackageScanner: IsAppInstalledAsync: " + 
+                    protocolName + " " + packageFamilyName + " " + appInstalled);
+
+                return appInstalled;
+
+            } catch (Exception e)
+            {
+                Debug.WriteLine("PackageScanner: IsAppInstalledAsync: Error: " + e);
+                return false;
+            }
+        }
+    }
+}
